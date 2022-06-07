bf45473a773d68197930e511d63658993872cc06
Anrian Sihotang <anrishotang91@gmail.com>
2022-06-08 00:52:38 +0700

    create jumbotron

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
-
+// import { Container } from "react-bootstrap";
+import HeroDestination from "../components/HeroDestination";
 class Destination extends Component {
 	render() {
 		return (
 			<div>
 				<Header />
-				<h1>destinatisi nih</h1>
+				<HeroDestination />
 			</div>
 		)
 	}
