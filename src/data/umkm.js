06e0b15ab8ed5e60c5ff0468633fc109e3404741
LuckyPice <106434753+LuckyPice@users.noreply.github.com>
2022-06-09 19:17:06 +0700

    umkm

---
+++ src/data/umkm.js
@@ -0,0 +1,56 @@
+import jengalaimg from '../images/content/jenggala.webp';
+import kunangimg from '../images/content/kunang-fix.webp';
+import maharaniimg from '../images/content/maharani.webp';
+import masonimg from '../images/content/mason chocolates.jpg';
+import Pithecanthropusimg from '../images/content/a.jpg';
+import VDesignimg from '../images/content/vdesign.webp';
+import taluimg from '../images/content/talu.jpg';
+
+
+const UM = [
+	{
+		nama: "Jenggala",
+		gambar: jengalaimg,
+        paragraf : "Bingung cari hadiah untuk para ibu rumah tangga? Kayaknya Sobat Pesona harus lihat produk Jenggala nih! Dijamin para ibu bakal kegirangan! Karena produk keramik Jenggala dibentuk dengan sangat elegan, artistik, unik, dan penuh warna. Tersedia beragam produk perabotan rumah, peralatan meja, kamar mandi dan spa, serta kotak hadiah. Setiap bentuk dan ukiran estetik dari produk keramik Jenggala menggambarkan kekhasan Bali seperti ukiran Kelapa, Ingka, Griya, Patra Punggel, Bambu, dan masih banyak lagi. Tidak heran UMKM yang berdiri sejak 1976 ini mendapatkan penghargaan dari UNESCO di tahun 2010. Yuk segera meluncur ke laman situs dan akun instagram Jenggala!"
+
+	},
+	{
+		nama: "KunangKunangku Bali",
+		gambar: kunangimg,
+        paragraf : "Sobat Pesona perlu hadiah untuk si kecil? Coba cek boneka di KunangKunangku Bali, yuk! Dijamin, si kecil bakal peluk-peluk terus boneka empuk dan lucu buatan tangan para pengrajin di KunangKunangku Bali. Enggak cuman anak-anak aja, boneka-boneka ini pun cocok untuk orang dewasa yang suka boneka.  Bisa digunakan baik untuk dekorasi kamar atau mobil, desain dan bahannya pun dibuat sedemikian rupa hingga terasa lembut dan nyaman dipeluk. Yuk, mampir ke laman situs atau akun instagram KunangKunangku Bali di sini!"
+
+	},
+	{
+		nama: "Maharani Handicraft ",
+		gambar: maharaniimg,
+        paragraf : "Menghadiahi orang tersayang dengan aksesoris buatan tangan pengrajin yang lihai bisa menjadi pilihan oleh-oleh selanjutnya, nih. Maharani Handicraft menyuguhkan aksesoris berbahan-bahan alami seperti batu permata, besi, dan kayu yang disulap oleh para pengrajin handal menjadi anting, cincin, gelang, serta kalung. Ukiran yang sangat artistik dan berkelas menjadikan produk-produk Maharani Handicraft diborong oleh negara maju seperti Amerika, Jerman dan Inggris.Selain aksesoris, para pengrajin asli Bali ini juga memproduksi dekorasi rumah, peralatan dapur, dan dekorasi meja. Pokoknya, produk Maharani Handicraft cocok banget untuk bingkisan kado atau buah tangan. Yuk cek katalog produknya di laman website dan akun instagram Maharani Handicraft."
+
+	},
+	{
+		nama: "Mason Chocolates ",
+		gambar: masonimg,
+        paragraf : "Hayo siapa di antara Sobat Pesona yang pecinta coklat? Tahu nggak nih, Indonesia merupakan negara penghasil coklat nomor 4 di dunia, setelah Pantai Gading, Ghana dan Ekuador. Produk lokal kita juga banyak yang punya cita rasa unik dan mendunia lho, salah satunya Mason Chocolates.Dibuat dari bahan-bahan berkualitas hasil perkebunan lokal, Mason Chocolate menyajikan berbagai macam coklat seperti coklat tipis, coklat kacang berlapis, coklat truffle dan praline, coklat batang, serta bubuk coklat untuk memasak. Selain mengandung antioksidan alami yang tinggi, coklat juga mampu membantu menurunkan tekanan darah dan meningkatkan mood kita. Penasaran mau coba? Sobat Pesona bisa pesan ke laman situs dan akun instagram Mason Chocolates sekarang juga!"
+
+	},
+	{
+		nama: "Pithecanthropus",
+		gambar: Pithecanthropusimg ,
+        paragraf : "Sobat Pesona ingin mencoba perpaduan Batik jaman dahulu dengan model baju kekinian? Mampir ke Pithecanthropus! UMKM lokal ini menyajikan produk-produk pakaian batik berkualitas dengan mengangkat motif batik khas warisan Indonesia tempo dulu, lho. Mengenakan batik ini serasa seperti kembali ke masa lalu! Dan ternyata, motif batik ini juga menginspirasi banyak desainer internasional.  Nah selain pakaian batik, tersedia juga selendang kain batik, produk dekorasi rumah, aksesoris hingga barang-barang antik. Sobat Pesona tertarik untuk membeli? Langsung saja kunjungi laman website dan akun instagram Pithecanthropus, ya!"
+
+	},
+	{
+		nama: "V Design",
+		gambar: VDesignimg,
+        paragraf : "Mau mendekor rumah dengan nuansa seperti di hotel bintang 5? V Design punya solusinya! Ada banyak bentukan keramik estetik dan bercorak seperti wadah sabun cair, vas bunga, mangkok buah-buahan, tempat lilin dan masih banyak lagi.  Produk andalan V Design terdiri dari lampu-lampu, aksesoris kamar hotel, peralatan kamar mandi, serta perabotan spa. Yang  bikin makin spesial, produk produk ini memiliki ukiran atau pahatan yang khas Bali. Kalau Sobat Pesona ingin terus berasa di Bali meskipun sedang di rumah, segera pesan produk V Design di laman website atau akun instagram nya ya! Wah, ternyata ada banyak banget nih UMKM lokal yang keren dan super kreatif di Bali yang produknya wajib buat dibeli. Yang pastinya, Sobat Pesona nggak akan rugi beli buah tangan dari jenama-jenama di atas untuk orang-orang tersayang. Selain dapat produk berkualitas, Sobat Pesona juga turut mendukung program pemerintah yaitu Beli Kreatif Lokal dan Beli Buatan Indonesia. Nah, jangan lupa juga, bagi Sobat Pesona yang ingin keluar rumah untuk selalu menerapkan protokol kesehatan 6M, seperti memakai masker, mencuci tangan, menjaga jarak, menjauhi kerumunan, mengurangi mobilitas, dan menghindari makan bersama. Dan yang nggak kalah pentingnya, yuk segera lakukan vaksinasi demi melindungi orang-orang yang kita cintai! Cari tau juga informasi tentang pariwisata Indonesia di akun Instagram @pesonaid_travel, Facebook @pesonaid_travel, dan kunjungi website www.indonesia.travel ya!"
+
+	},
+	{
+		nama: "Talu",
+		gambar: taluimg,
+        paragraf : "Kalau Sobat Pesona ingin jalan-jalan di Bali mengenakan pakaian sehari-hari yang Indonesia banget, Talu solusinya! Model batik dari Talu menawarkan motif batik klasik Indonesia dengan bahan kain yang nyaman dan sejuk. Motifnya pun cenderung simpel dan santai. Model bajunya pun modis dan kekinian. Warna-warna baju yang kalem, pas dipasangkan dengan pakaian bawahan apapun. Jadi, cocok deh untuk bersantai-santai di pantai Bali. Yuk, intip katalognya di laman situs dan akun instagram Talu!"
+
+	}
+
+]
+
+export default  UM;
